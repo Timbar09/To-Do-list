fa74e341147a65ba14e19aea1d326cb8bd1cd5c7
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-01-17 12:55:08 +0200

    Add tasks array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const tasks = [
+  {
+    description: 'Develop a todo list app',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Submit before end of day',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Get some rest',
+    completed: false,
+    index: 3,
+  },
+];
