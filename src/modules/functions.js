118d5955a582b6c69c5db1a54bcc57a79c7cf037
Oscah01 <pipkillerinfo@gmail.com>
2023-01-26 18:19:53 +0300

    add remove function local storage test

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,11 +1,11 @@
-export const taskContainer = document.querySelector('.list__tasks');
+export const taskContainer = document.querySelector(".list__tasks");
 
 // Get the tasks from local storage
 export const getTasks = () => {
   let tasks;
 
-  if (localStorage.getItem('tasks')) {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
+  if (localStorage.getItem("tasks")) {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
   } else {
     tasks = [];
   }
@@ -15,38 +15,38 @@ export const getTasks = () => {
 
 // Add a task to local storage
 export const storeTask = (tasks) => {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 export const render = (tasks) => {
-  taskContainer.innerHTML = '';
+  taskContainer.innerHTML = "";
 
   tasks.forEach((task) => {
-    const todoTask = document.createElement('li');
+    const todoTask = document.createElement("li");
     todoTask.className = `list__task padding-x flex  flex-ai-c ${task.checked}`;
     todoTask.id = task.index;
 
-    const checkbox = document.createElement('span');
-    checkbox.className = 'checkbox';
+    const checkbox = document.createElement("span");
+    checkbox.className = "checkbox";
 
-    const taskEditWrap = document.createElement('div');
-    taskEditWrap.className = 'list__task-edit-wrap';
-    taskEditWrap.style.setProperty('--width', `${task.description.length}ch`);
+    const taskEditWrap = document.createElement("div");
+    taskEditWrap.className = "list__task-edit-wrap";
+    taskEditWrap.style.setProperty("--width", `${task.description.length}ch`);
 
-    const taskEdit = document.createElement('input');
-    taskEdit.className = 'list__task-edit';
-    taskEdit.type = 'text';
+    const taskEdit = document.createElement("input");
+    taskEdit.className = "list__task-edit";
+    taskEdit.type = "text";
     taskEdit.value = task.description;
     taskEdit.maxLength = 40;
 
     taskEditWrap.appendChild(taskEdit);
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = 'delete';
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete";
     deleteBtn.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
 
-    const moveBtn = document.createElement('button');
-    moveBtn.className = 'list__task-move btn';
+    const moveBtn = document.createElement("button");
+    moveBtn.className = "list__task-move btn";
     moveBtn.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
 
     todoTask.appendChild(checkbox);
@@ -68,14 +68,17 @@ export const addTask = (tasks, task) => {
 export const removeTask = (tasks, taskIndex) => {
   tasks.splice(taskIndex, 1);
 
-    tasks.forEach((task, i) => {
-      task.index = i + 1;
-    });
+  tasks.forEach((task, i) => {
+    task.index = i + 1;
+  });
+  return tasks;
 };
 
 export const editTask = (tasks, target) => {
-  if (target.classList.contains('list__task-edit')) {
-    const taskIndex = tasks.findIndex((task) => task.index.toString() === target.closest('.list__task').id);
+  if (target.classList.contains("list__task-edit")) {
+    const taskIndex = tasks.findIndex(
+      (task) => task.index.toString() === target.closest(".list__task").id
+    );
 
     tasks[taskIndex].description = target.value;
 
--- src/modules/tests/remove.test.js
+++ src/modules/tests/remove.test.js
@@ -7,20 +7,15 @@ import Task from '../taskClass.js';
 
 // AddTask tests
 describe('Remove tasks', () => {
-  test('is task removed from the DOM', () => {
-    const newTask = new Task('task');
-    const list = [];
-
-    expect(addTask(list, newTask)).toHaveLength(1);
-  });
-
-  test('is local storage updated', () => {
-    const newTask = new Task('storageTask');
-    const list = [];
-    addTask(list, newTask);
-
-    storeTask(list);
-
-    expect(getTasks()).toHaveLength(1);
-  });
+    test('is task added to DOM', () => {
+        const list = [
+          {description: "hdfhdo", complete: true, index: 1, checked: "checked"},
+          {description: "task1", complete: false, index: 2, checked: ""},
+          {description: "task2", complete: false, index: 3, checked: ""},
+          {description: "task4 ", complete: false, index: 4, checked: ""},
+          {description: "task5", complete: false, index: 5, checked: ""}
+         ];
+    
+        expect(removeTask(list, 2)).toHaveLength(4);
+      });
 });
