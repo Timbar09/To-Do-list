d09a77e7327351b7a862b43108c5afa1e398f40d
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-01-27 14:20:23 +0200

    Add DOM test for clearAllCompleted function

--- src/modules/tests/crudtests.test.js
+++ src/modules/tests/crudtests.test.js
@@ -1,70 +1,115 @@
 /**
  * @jest-environment jsdom
  */
+
 import { editTask, getTasks, storeTask } from '../functions.js';
-import { checkOutTask } from '../taskStatus.js';
+import { checkOutTask, clearAllCompleted } from '../taskStatus.js';
 
+// editTask function tests
 describe('Edit task', () => {
-    test('is the task edited on DOM', () => {
-        const list = [
-            {
-              description: 'task5', complete: true, index: 0, checked: 'checked',
-            }
-          ];
-        const targetObj = {
-            value: 'taskfive'
-        }
-
-      const changedDescription = editTask(list, targetObj, 0)[0].description;
-    
-        expect(changedDescription).toBe('taskfive');
-    });
-    test('is the task edited on LocalStorage', () => {
-        const list = [
-            {
-              description: 'task5', complete: true, index: 0, checked: 'checked',
-            }
-          ];
-        const targetObj = {
-            value: 'taskfive'
-        }
-      editTask(list, targetObj, 0)
-      storeTask(list)
-      const storedList = getTasks()
-      const changedDescription = storedList[0].description;
-    
-        expect(changedDescription).toBe('taskfive');
-    });
-  });
-
-  describe('update task complete status', () => {
-
-   test('is complete status changed on the DOM', () => {
+  test('is the task edited on DOM', () => {
     const list = [
       {
-        description: 'task5', complete: false, index: 0, checked: '',
-      }
+        description: 'task5',
+        complete: true,
+        index: 0,
+        checked: 'checked',
+      },
     ];
+    const targetObj = {
+      value: 'taskfive',
+    };
 
-    checkOutTask(list, 0)
-    const completeStatus = list[0].complete
+    const changedDescription = editTask(list, targetObj, 0)[0].description;
 
-    expect(completeStatus).toBe(true);
-   })
-   
-   test('is complete status changed on localStorage', () => {
+    expect(changedDescription).toBe('taskfive');
+  });
+
+  test('is the task edited on LocalStorage', () => {
     const list = [
       {
-        description: 'task5', complete: false, index: 0, checked: '',
-      }
+        description: 'task5',
+        complete: true,
+        index: 0,
+        checked: 'checked',
+      },
+    ];
+    const targetObj = {
+      value: 'taskfive',
+    };
+
+    editTask(list, targetObj, 0);
+    storeTask(list);
+    const storedList = getTasks();
+    const changedDescription = storedList[0].description;
+
+    expect(changedDescription).toBe('taskfive');
+  });
+});
+
+// checkOutTask function tests
+describe('update task complete status', () => {
+  test('is complete status changed on the DOM', () => {
+    const list = [
+      {
+        description: 'task5',
+        complete: false,
+        index: 0,
+        checked: '',
+      },
     ];
 
-    checkOutTask(list, 0)
-    storeTask(list)
-    const storedList = getTasks()
-    const completeStatus = storedList[0].complete
+    checkOutTask(list, 0);
+    const completeStatus = list[0].complete;
 
     expect(completeStatus).toBe(true);
-   })
-
   });
+
+  test('is complete status changed on localStorage', () => {
+    const list = [
+      {
+        description: 'task5',
+        complete: false,
+        index: 0,
+        checked: '',
+      },
+    ];
+
+    checkOutTask(list, 0);
+    storeTask(list);
+    const storedList = getTasks();
+    const completeStatus = storedList[0].complete;
+
+    expect(completeStatus).toBe(true);
+  });
+});
+
+// clearAllTasks function tests
+describe('Filtered tasks', () => {
+  test('are the cleared tasks removed from the DOM', () => {
+    const list = [
+      {
+        description: 'task1',
+        complete: false,
+        index: 1,
+        checked: '',
+      },
+      {
+        description: 'task3',
+        complete: true,
+        index: 2,
+        checked: 'checked',
+      },
+      {
+        description: 'task5',
+        complete: false,
+        index: 3,
+        checked: '',
+      },
+    ];
+
+    const remainingTasks = clearAllCompleted(list);
+
+    expect(remainingTasks).toHaveLength(2);
+  });
+});
